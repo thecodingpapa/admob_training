a2c2b8da9c9aa7d118fa5aea97accaa34272e019
Victorer <victor@myicellar.com>
2022-01-19 09:44:47 +0900

    - Banner Ads

--- lib/screens/inline_ads_screen.dart
+++ lib/screens/inline_ads_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:admob_training/dummy.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class InlineAdsScreen extends StatefulWidget {
   const InlineAdsScreen({Key? key}) : super(key: key);
@@ -11,27 +12,90 @@ class InlineAdsScreen extends StatefulWidget {
 }
 
 class _InlineAdsScreenState extends State<InlineAdsScreen> {
+  bool _isLoaded = false;
+  BannerAd? myBanner;
+
+  Size get _screenSize => MediaQuery.of(context).size;
+  AdSize? _adSize;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loadAd();
+  }
+
+  void _loadAd() {
+    AdSize size =
+        AdSize.getInlineAdaptiveBannerAdSize(_screenSize.width.truncate(), 200);
+
+    myBanner = BannerAd(
+        adUnitId: BannerAd.testAdUnitId,
+        size: size,
+        request: AdRequest(),
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) async {
+            print('Inline adaptive banner loaded: ${ad.responseInfo}');
+            BannerAd bannerAd = (ad as BannerAd);
+            final AdSize? size = await bannerAd.getPlatformAdSize();
+            if (size == null) {
+              print('Error: getPlatformAdSize() returned null for $bannerAd');
+              return;
+            }
+            setState(() {
+              myBanner = bannerAd;
+              _isLoaded = true;
+              _adSize = size;
+            });
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            ad.dispose();
+            print('Ad failed to load: $error');
+          },
+          onAdOpened: (Ad ad) => print('Ad opened.'),
+          onAdClosed: (Ad ad) => print('Ad closed.'),
+          onAdImpression: (Ad ad) => print('Ad impression.'),
+        ));
+    myBanner!.load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
         itemBuilder: (context, index) {
+          if (index == 3) {
+            if (myBanner != null && _adSize != null && _isLoaded) {
+              return Container(
+                alignment: Alignment.center,
+                height: _adSize!.height.toDouble(),
+                width: _adSize!.width.toDouble(),
+                child: AdWidget(
+                  ad: myBanner!,
+                ),
+              );
+            } else {
+              return Container();
+            }
+          }
+
+          int realIndex = (index < 3) ? index : index - 1;
+
           return ListTile(
             leading: CircleAvatar(
-              foregroundImage: NetworkImage(dummies[index].profile),
+              foregroundImage: NetworkImage(dummies[realIndex].profile),
             ),
             title: Text(
-              dummies[index].name,
+              dummies[realIndex].name,
               overflow: TextOverflow.ellipsis,
             ),
             subtitle: Text(
-              dummies[index].sub,
+              dummies[realIndex].sub,
               overflow: TextOverflow.ellipsis,
             ),
             trailing: const Icon(Icons.favorite),
           );
         },
-        itemCount: dummies.length,
+        itemCount: dummies.length + 1,
       ),
     );
   }
--- lib/screens/top_ads_screen.dart
+++ lib/screens/top_ads_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:admob_training/dummy.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class TopAdsScreen extends StatefulWidget {
   const TopAdsScreen({Key? key}) : super(key: key);
@@ -9,27 +12,108 @@ class TopAdsScreen extends StatefulWidget {
 }
 
 class _TopAdsScreenState extends State<TopAdsScreen> {
+  BannerAd? _bannerAd;
+  InterstitialAd? _interstitialAd;
+  bool _isLoaded = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loadAd();
+    _loadInterstitialAd();
+  }
+
+  void _loadInterstitialAd() {
+    InterstitialAd.load(
+        adUnitId: InterstitialAd.testAdUnitId,
+        request: AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+            _interstitialAd = ad;
+            _interstitialAd!.show();
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error');
+          },
+        ));
+  }
+
+  void _loadAd() async {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    final AnchoredAdaptiveBannerAdSize? size =
+        await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+            screenSize.width.truncate());
+
+    if (size == null) {
+      print('Unable to get height of anchored banner.');
+      return;
+    }
+
+    _bannerAd = BannerAd(
+      adUnitId: Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/6300978111'
+          : 'ca-app-pub-3940256099942544/2934735716',
+      size: size,
+      request: AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+            _isLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          print('Anchored adaptive banner failedToLoad: $error');
+          ad.dispose();
+        },
+      ),
+    );
+
+    _bannerAd!.load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              foregroundImage: NetworkImage(dummies[index].profile),
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            ListView.builder(
+              padding: EdgeInsets.only(
+                  bottom: (_bannerAd != null && _isLoaded)
+                      ? _bannerAd!.size.height.toDouble()
+                      : 0),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    foregroundImage: NetworkImage(dummies[index].profile),
+                  ),
+                  title: Text(
+                    dummies[index].name,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    dummies[index].sub,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  trailing: const Icon(Icons.favorite),
+                );
+              },
+              itemCount: dummies.length,
             ),
-            title: Text(
-              dummies[index].name,
-              overflow: TextOverflow.ellipsis,
-            ),
-            subtitle: Text(
-              dummies[index].sub,
-              overflow: TextOverflow.ellipsis,
-            ),
-            trailing: const Icon(Icons.favorite),
-          );
-        },
-        itemCount: dummies.length,
+            if (_bannerAd != null && _isLoaded)
+              Container(
+                color: Colors.green,
+                width: _bannerAd!.size.width.toDouble(),
+                height: _bannerAd!.size.height.toDouble(),
+                child: AdWidget(
+                  ad: _bannerAd!,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
